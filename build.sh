f8c697f1f2744e7abf66cd0d1ea72d0348cc4fea
CCRICE <2371092140@qq.com>
2025-08-04 13:07:49 +0800

    fix error on github action

--- build.sh
+++ build.sh
@@ -18,30 +18,12 @@ for arg in "$@"; do
 done
 
 echo "正在运行 Flutter 构建..."
-if [[ " ${FLUTTER_ARGS[@]} " =~ " --no-web-resources-cdn " ]]; then
-  export NO_CDN=true
-fi
-if [[ " ${FLUTTER_ARGS[@]} " =~ " --debug " ]]; then
-  export DEBUG=true
-fi
 
-# flutter "${FLUTTER_ARGS[@]}"
 flutter build web --no-web-resources-cdn
-# shellcheck disable=SC1073
-if "%~1"=="--debug" set DEBUG=true
-flutter !FLUTTER_ARGS!
 
-if [ $? -ne 0 ]; then
-  echo "Flutter 构建失败"
-  exit 1
-fi
 
 echo "正在注入加载页面..."
-dart run tools/inject_loading_screen.dart "${INJECT_ARGS[@]}"
+dart run tools/inject_loading_screen.dart "--local-assets"
 
-if [ $? -ne 0 ]; then
-  echo "注入脚本失败"
-  exit 1
-fi
 
 echo "构建和注入完成!"
